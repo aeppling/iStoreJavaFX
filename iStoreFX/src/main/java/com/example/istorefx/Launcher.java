7104b92770db2de94c61e87c242b239e1aab722f
Léo Richez <leo.richez59@gmail.com>
2023-02-03 14:33:59 +0100

    add .jar executable file

---
+++ iStoreFX/src/main/java/com/example/istorefx/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.istorefx;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Replace "Main" with the name of the class that extends Application
+        // See https://stackoverflow.com/a/52654791/3956070 for explanation
+        HelloApplication.main(args);
+    }
+}
